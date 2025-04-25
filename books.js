00127834794cf9ca978a5b26025f69221989e172
Miguel Galindo <miguelgalindo1214@gmail.com>
2025-04-25 16:59:25 -0400

    Adding books dynamically

--- books.js
+++ books.js
@@ -1,4 +1,38 @@
+function renderBooks() {
+  const booksWrapper = document.querySelector('.books');
 
+  const books = getBooks();
+  const booksHtml = books.map(book => {
+    return`<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span>$${book.originalPrice.toFixed(2)}
+    </div>
+  </div>`
+  }).join("");
+
+
+
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksWrapper);
+  
+}
+
+setTimeout(() => {
+  renderBooks();
+})
 
 // FAKE DATA
 function getBooks() {
